731d4b6c158cab5a71eb58aa3f2e03a1a070b823
MegaDewi2 <50202220+MegaDewi2@users.noreply.github.com>
2025-01-28 20:08:14 +0700

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -20,9 +20,9 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
    def update_r(self):
        keys = key.get_pressed()
-       if keys[K_RIGHT] and self.rect.y > 5:
+       if keys[K_UP] and self.rect.y > 5:
            self.rect.y -= self.speed
-       if keys[K_LEFT] and self.rect.y < win_height - 80:
+       if keys[K_DOWN] and self.rect.y < win_height - 80:
            self.rect.y += self.speed
    def update_l(self):
        keys = key.get_pressed()
